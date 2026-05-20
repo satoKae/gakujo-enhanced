49d8d46e45543cb777e64326fd88ba69a66764e7
satokae <208755468+satokae@users.noreply.github.com>
2026-05-20 14:51:21 +0900

    feat: キーの追加 [, ]

--- src/features/addKeyboardShortcuts.ts
+++ src/features/addKeyboardShortcuts.ts
@@ -1,10 +1,18 @@
+import { focusSidebar } from '../utils/focusSidebar';
+
 const preventScroll: FocusOptions = { preventScroll: true };
+
+const focusableSelector = ['a[href]', 'input:not([type=hidden])', 'select', 'button', 'textarea']
+  .map((s) => s + ':not([disabled])')
+  .join(',');
 
 export function addKeyboardShortcuts(): void {
   const headerInformation = document.getElementsByClassName('c-header-information')?.[0];
   const headerInformationButton = document.getElementsByClassName('c-header-btn-information')?.[0];
+  const mainElement = document.getElementsByTagName('main')?.[0];
   let focusedOn: undefined | null | Element;
 
+  // Ctrl + I
   if (headerInformation instanceof HTMLElement && headerInformationButton instanceof HTMLAnchorElement) {
     document.addEventListener('keyup', (e) => {
       if (e.ctrlKey && !e.altKey && !e.shiftKey && e.code === 'KeyI') {
@@ -21,4 +29,20 @@ export function addKeyboardShortcuts(): void {
       }
     });
   }
+
+  // [, ]
+  document.addEventListener('keyup', (e) => {
+    if (e.ctrlKey || e.altKey || e.shiftKey) {
+      return;
+    }
+
+    if (e.code === 'BracketRight') {
+      focusSidebar();
+    } else if (e.code === 'Backslash') {
+      const focusableElement = mainElement?.querySelector(focusableSelector);
+      if (focusableElement instanceof HTMLElement) {
+        focusableElement.focus();
+      }
+    }
+  });
 }
